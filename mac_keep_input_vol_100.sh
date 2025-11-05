55f2f96e79fc00b1b079d346fee177d7b7d0b9a1
Hafiz Faizan Ul Hassan <faizanalhassan@gmail.com>
2025-11-05 19:42:02 +0500

    Refactor volume monitoring script to improve logging and reset timer functionality

--- mac_keep_input_vol_100.sh
+++ mac_keep_input_vol_100.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 last_vol=-1
 counter=0
+last_change_time=$(date +%s)  # record start time
 
 while true; do
   ((counter++))
@@ -8,13 +9,29 @@ while true; do
 
   vol=$(osascript -e "input volume of (get volume settings)")
   if [ "$vol" -ne "$last_vol" ]; then
-    echo "$(date): volume changed to $vol" >> ~/input_vol_out.txt
+    if [ "$last_vol" -ne -1 ]; then
+      echo "$(date): input volume changed from $last_vol to $vol" 
+    else
+      echo "$(date): initial input volume detected: $vol" 
+    fi
+
     if [ "$vol" -lt 100 ]; then
       osascript -e "set volume input volume 100"
+      echo "$(date): input volume adjusted from $vol to 100" 
+      last_vol=100
+    else
+      last_vol=$vol
     fi
-    last_vol=$vol
+
+    last_change_time=$(date +%s)  # reset timer
     sleep 1
   else
+    current_time=$(date +%s)
+    idle_time=$(( current_time - last_change_time ))
+    if [ "$idle_time" -ge 1800 ]; then
+      echo "$(date): No volume change for 5 minutes. Exiting." 
+      break
+    fi
     sleep 5
   fi
 done
